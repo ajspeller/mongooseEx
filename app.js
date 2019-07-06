c72336de9f65bed6669b37034d361d45bf78e705
AJ Speller <ajspeller@gmail.com>
2019-07-06 03:55:29 -0400

    Basic GET route and setup the .http rest client to test endpoints

--- app.js
+++ app.js
@@ -19,6 +19,31 @@ mongoose.connect(db, { useNewUrlParser: true }, (err, client) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.send('hola');
+});
+
+app.get('/books', (req, res) => {
+  console.log(`Retrieve all books!`);
+  Book.find({}).exec((err, books) => {
+    if (err) {
+      return res.send({ error: err });
+    }
+    res.json(books);
+  });
+});
+
+app.get('/books/:id', (req, res) => {
+  console.log(`Retrieve a single book`);
+  const { id: _id } = req.params;
+  Book.findOne({ _id }).exec((err, book) => {
+    if (err) {
+      return res.send({ error: err });
+    }
+    res.json(book);
+  });
+});
+
 app.listen(PORT, () =>
   console.log(`Webserver started successfully on port ${PORT}`)
 );
